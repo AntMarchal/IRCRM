8229698c008389d76a8241da5ff25c5b9a6eeee5
valentintissot <valentin.tissotdaguette@gmail.com>
2019-10-21 15:31:53 +0200

    .

--- BMTOG Problem Set 5/BMTOG_PS5_Ex2.m
+++ BMTOG Problem Set 5/BMTOG_PS5_Ex2.m
@@ -6,13 +6,16 @@
 %======================= OUYANG Tonglin, GIRO Tomas ======================%
 %=========================================================================%
 
-close all; clear; clc; format short; warning('off')
+close all; clear; clc; format long; warning('off')
+
+% Fix the random seed
+rng(1)
 
 %% 0. Setup 
 
 beta = -0.86; b = 0.09 * abs(beta); sigma = 0.0148; r_0 = 0.095;
 
-T = 2; N_T = T * 252; D_t = T / N_T; N_sim = 10000;
+T = 2; N_T = T * 252; D_t = T / N_T; N_sim = 1e4;
 
 grid = linspace(0,T,N_T+1);
 
@@ -21,11 +24,14 @@ grid = linspace(0,T,N_T+1);
 % Store the terms exp(beta*t_i) in a vector 
 Y = exp(beta * grid');
 
-% Exact mean and variance of the process
-Mu = r_0 * Y + b/beta * (Y-1); Var = sigma^2*(Y.^2-1)/(2*beta);
+% Mean vector of (r_t_1,...,r_t_N_T)
+Mu = r_0 * Y + b/beta * (Y-1);
 
-% Resulting trajectories for the short rate 
-r = Mu + sqrt(Var).* [ones(1,N_sim);randn(N_T,N_sim)];
+% Covariance matrix of (r_t_1,...,r_t_N_T)
+Sigma = sigma^2*(Y*Y' - (tril(Y*(1./Y)') + tril(Y*(1./Y)',-1)'))/(2*beta);
+
+% Exactly simulated sample paths for the short rate 
+r =  mvnrnd(Mu,Sigma,N_sim)';
 
 %% II. Evolution of the Bank Account and obtained price
 
@@ -39,7 +45,7 @@ fprintf('\nMonte Carlo Price of the ZCB Bond: %2.5f\n',MC_Price)
 
 %% III. Plots
 
-figure 
+%figure 
 
 subplot(2,1,1); plot(grid,r(:,1:200),'Linewidth',1.5); 
 xlabel('t');    title('Short Rate')
