dd0a622c9c1188b4a57cb900106fcac83fb7ca36
girotomas <girotomas@gmail.com>
2019-12-17 22:17:38 +0100

    Add files via upload

--- BMTOG Problem Set 12/ex3/ex3.m
+++ BMTOG Problem Set 12/ex3/ex3.m
@@ -1,31 +1,26 @@
 clear all
 %% b) 
-global r P0 ;
+global r P0 ; % r is the initial value for r; P0 is the value of the riskless bond
 r = 0.077/0.96; % initial value for r (not specified in problem set)
 P0 = p0(1);
 
 delta = 0.5;
 
-Jhandle = @(x) J(x);
-JJ = quadgk(Jhandle,0,1);
-P1 =  I(1) + (1-delta) * JJ;
-fprintf('P0:  %f\n', P0)
-fprintf('J:  %f\n', JJ)
-fprintf('I:  %f\n',I(1))
+Jhandle = @(x) J(x); % we define a handle for the function J
+JJ = quadgk(Jhandle,0,1); % we compute the C* from the report
+P1 =  I(1) + (1-delta) * JJ; % we compute the price p1
 fprintf('P1:  %f\n', P1)
 
 
 %% c)
-P0 = p0(5);
+P0 = p0(5); % we change the value of P0 as we have a new maturity
 Jhandle = @(x) J(x);
 JJ = quadgk(Jhandle,0,5);
-denom = 0;
+denom = 0;  % we compute the denominator of the formula on page 677
 for i=1:10
-    denom = denom + 0.5 * I(i/2);
+    denom = denom + 0.5 * I(i/2); 
 end
 x = (delta * JJ ) / denom;
-fprintf('numer*:  %f\n', delta*JJ)
 
-fprintf('denom*:  %f\n', denom)
 
 fprintf('x*:  %f\n', x)
